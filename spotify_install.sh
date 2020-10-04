e66081fe5a52e24231c8771548733e80237ed0fb
Tapan Pareek <tapanpareek20@gmail.com>
2020-10-04 09:51:20 +0530

    Add files via upload

---
+++ spotify_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install make
+sudo apt install build-essential
+sudo apt-get install libcurl4-gnutls-dev
+sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+git clone https://github.com/kaizoku49/spotify-adblock-linux.git
+cd spotify-adblock-linux
+wget -O cef.tar.bz2 http://opensource.spotify.com/cefbuilds/cef_binary_80.0.8%2Bgf96cd1d%2Bchromium-80.0.3987.132_linux64_minimal.tar.bz2
+tar -xf cef.tar.bz2 --wildcards '*/include' --strip-components=1
+make
+sudo make install
