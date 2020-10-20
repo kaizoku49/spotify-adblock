f3eae08d9e5189ace7f0b86e3b07a7ec56797414
Tapan Pareek <tapanpareek20@gmail.com>
2020-10-20 06:06:50 +0530

    Update spotify_install.sh

--- spotify_install.sh
+++ spotify_install.sh
@@ -5,7 +5,7 @@ sudo apt install build-essential
 sudo apt install curl 
 sudo apt-get install libcurl4-gnutls-dev
 sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client
 git clone https://github.com/kaizoku49/spotify-adblock-linux.git
